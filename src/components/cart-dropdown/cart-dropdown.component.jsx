f8a16239009f3afb7bec66200b0341c59abd3b2e
devikapandya1 <devikajoshi2923@gmail.com>
2023-10-31 15:14:52 +0530

    change

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,15 @@
 import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { CartContext } from '../../contexts/cart.contexts';
 
-import { CartContext } from '../../contexts/cart.context';
 
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 
 import {
-  CartDropdownContainer,
-  EmptyMessage,
-  CartItems,
+    CartDropdownContainer,
+    CartItems,
+    EmptyMessage,
 } from './cart-dropdown.styles';
 
 const CartDropdown = () => {
