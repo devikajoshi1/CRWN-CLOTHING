ab3b39b24ea95a781641c8e93887862d8c52bc44
devikapandya1 <devikajoshi2923@gmail.com>
2023-10-31 13:33:28 +0530

    change

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { CartProvider } from './contexts/cart.contexts';
-import './index.scss';
+
 import App from './App';
+import { CartProvider } from './contexts/cart.contexts';
+import { CategoriesProvider } from './contexts/categories.context';
 import { UserProvider } from './contexts/user.contexts';
-import reportWebVitals from './reportWebVitals';
- import { render } from '@testing-library/react';
-import {CategoriesProvider} from './contexts/categories.context';
+
+
+import './index.scss';
+
 const rootElement = document.getElementById('root');
 
 render(
@@ -16,18 +18,11 @@ render(
       <UserProvider>
         <CategoriesProvider>
           <CartProvider>
-          <App />
+            <App />
           </CartProvider>
-       </CategoriesProvider>
+        </CategoriesProvider>
       </UserProvider>
-     
     </BrowserRouter>
-    </React.StrictMode>,
-
-    rootElement
+  </React.StrictMode>,
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
